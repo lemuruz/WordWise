c72f8c3d03fd1c9c0fc76a55683132ce14ef3a2a
BontyBo <gabane1950ultrapromax@gmail.com>
2025-02-02 11:39:22 +0700

    add words to database, modified model(word type to drop down)

--- WordWise/wordbank/models.py
+++ WordWise/wordbank/models.py
@@ -1,9 +1,37 @@
 from django.db import models
 
-# Create your models here.
 class wordBank(models.Model):
+    WORD_TYPES = [
+        ('noun', 'Noun'),
+        ('pronoun', 'Pronoun'),
+        ('verb', 'Verb'),
+        ('adjective', 'Adjective'),
+        ('adverb', 'Adverb'),
+        ('preposition', 'Preposition'),
+        ('conjunction', 'Conjunction'),
+        ('interjection', 'Interjection'),
+        ('determiner', 'Determiner'),
+        ('phrase', 'Phrase'),
+        ('modal verb', 'Modal Verb'),
+        ('article', 'Article'),
+        ('quantifier', 'Quantifier'),
+        ('exclamatory word', 'Exclamatory Word'),
+        ('gerund', 'Gerund'),
+        ('infinitive', 'Infinitive'),
+        ('relative pronoun', 'Relative Pronoun'),
+        ('relative adverb', 'Relative Adverb'),
+        ('prepositional phrase', 'Prepositional Phrase'),
+        ('conjunction adverb', 'Conjunction Adverb'),
+        ('correlative conjunction', 'Correlative Conjunction'),
+        ('conditional', 'Conditional'),
+        ('subjunctive mood', 'Subjunctive Mood'),
+        ('indefinite pronoun', 'Indefinite Pronoun'),
+        ('causative verb', 'Causative Verb'),
+        ('intensifier', 'Intensifier')
+    ]
+
     word = models.CharField(max_length=100, null=False)
-    word_type = models.CharField(max_length=50, null=False)
+    word_type = models.CharField(max_length=50, null=False, choices=WORD_TYPES)
     meaning = models.TextField()
     translates = models.CharField(max_length=100, null=False)
 
