83b77553a7eca4a977d22dc409fbc54b36043076
lemuruz <nakabodeepatiparn@gmail.com>
2025-02-06 14:43:55 +0700

    link user data to flashcard data(many to many) and add crossword algorithm

--- WordWise/user/admin.py
+++ WordWise/user/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Account
 
-admin.site.register(Account)
+class UserManage(admin.ModelAdmin):
+    filter_horizontal = ('flashcard',)  # Improves selection UI
+
+# Check if the model is already registered
+if not admin.site.is_registered(Account):
+    admin.site.register(Account, UserManage)
